ea73d830099f6cb12af527f24359991e43cc39f3
Bernardo <bernardo.mr120@gmail.com>
2025-01-14 17:01:26 +0000

    app.py file modificado para adicionar a ligação a DB e enviar o form para la /// dockerfile da imagem da app /// docker-compose da app e db juntas

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -22,22 +23,61 @@ def submit_data():
     n_of_sites = request.form['n_of_sites']
     upgrade_version = request.form['upgrade_version']
     remarks = request.form['remarks']
+
+    #setting up the connection configuration to the database
+    DB_HOST = 'database'
+    DB_PORT = 3306
+    DB_USER = 'root'
+    DB_PASSWORD = 'myDBpassword'
+    DB_NAME = 'metrics'
+
     
-    #Construct dcionary with all the data
-    metrics_data = {
-        'NofNodes': n_of_nodes,
-        'Market': market,
-        'Country': country,
-        'Customer': customer,
-        'Activity': activity,
-        'Product': product,
-        'Site': site,
-        'NofSites': n_of_sites,
-        'UpgradeVersion': upgrade_version,
-        'Remarks': remarks
-    }
-    print(metrics_data)
-    return "Form submited!"
+    try:
+        # Connect to the MySQL database
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+
+        # Create a cursor object to execute SQL queries
+        cursor = connection.cursor()
+
+        #Create the table if it doesn't exist
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS metrics (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            NofNodes INT,
+            Market VARCHAR(255),
+            Country VARCHAR(255),
+            Customer VARCHAR(255),
+            Activity VARCHAR(255),
+            Product VARCHAR(255),
+            Site VARCHAR(255),
+            NofSites INT,
+            UpgradeVersion VARCHAR(255),
+            Remarks VARCHAR(255)
+        )
+        """
+        cursor.execute(create_table_query)
+
+        # Insert the data into the database
+        insert_query = "INSERT INTO metrics (NofNodes, Market, Country, Customer, Activity, Product, Site, NofSites, UpgradeVersion, Remarks) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (n_of_nodes, market, country, customer, activity, product, site, n_of_sites, upgrade_version, remarks)
+        cursor.execute(insert_query, values)
+
+        # Commit the changes to the database
+        connection.commit()
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+        return "Form submitted successfully!"
+    
+    except Exception  as e:
+        return str(e), 500
 
 
 if __name__ == '__main__':
